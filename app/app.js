e946f7c73f7e52c0f9c25031458cbabba6afdb06
Przemysław <drazewski.przemyslaw@gmail.com>
2017-03-17 22:16:37 +0100

    Adjust boilerplate

--- app/app.js
+++ app/app.js
@@ -1,37 +1,13 @@
-/**
- * 
- * AngularJS Boilerplate
- * @description           Description
- * @author                Jozef Butko // www.jozefbutko.com/resume
- * @url                   www.jozefbutko.com
- * @version               1.1.7
- * @date                  March 2015
- * @license               MIT
- * 
- */
 ;(function() {
 
-
-  /**
-   * Definition of the main app module and its dependencies
-   */
   angular
     .module('boilerplate', [
       'ngRoute'
     ])
     .config(config);
 
-  // safe dependency injection
-  // this prevents minification issues
   config.$inject = ['$routeProvider', '$locationProvider', '$httpProvider', '$compileProvider'];
 
-  /**
-   * App routing
-   *
-   * You can leave it here in the config section or take it out
-   * into separate file
-   * 
-   */
   function config($routeProvider, $locationProvider, $httpProvider, $compileProvider) {
 
     $locationProvider.html5Mode(false);
@@ -61,12 +37,6 @@
 
   }
 
-
-  /**
-   * You can intercept any request or response inside authInterceptor
-   * or handle what should happend on 40x, 50x errors
-   * 
-   */
   angular
     .module('boilerplate')
     .factory('authInterceptor', authInterceptor);
@@ -95,10 +65,6 @@
     };
   }
 
-
-  /**
-   * Run block
-   */
   angular
     .module('boilerplate')
     .run(run);
--- app/controller.js
+++ app/controller.js
@@ -1,10 +1,3 @@
-/**
- * Main application controller
- *
- * You can use this controller for your whole app if it is small
- * or you can have separate controllers for each logical section
- * 
- */
 ;(function() {
 
   angular
@@ -13,24 +6,11 @@
 
   MainController.$inject = ['LocalStorage', 'QueryService'];
 
-
   function MainController(LocalStorage, QueryService) {
 
     // 'controller as' syntax
     var self = this;
-
-
-    ////////////  function definitions
-
-
-    /**
-     * Load some data
-     * @return {Object} Returned object
-     */
-    // QueryService.query('GET', 'posts', {}, {})
-    //   .then(function(ovocie) {
-    //     self.ovocie = ovocie.data;
-    //   });
+    
   }
 
 
